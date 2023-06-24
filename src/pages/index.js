ddde371b70698a67d4bd09ef400fb88d04cc2d86
Oussama Abderraouf Attia <92870205+OussamaAbderraoufAttia@users.noreply.github.com>
2023-06-24 23:15:25 +0100

    #Layout is done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,7 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Layout from '@/components/Layout';
+import Image from 'next/image';
+import developerPic from '../../public/images/profile/developer-pic-1.png';
 
 export default function Home() {
   return (
@@ -7,9 +10,23 @@ export default function Home() {
         <title>Oussama Abderraouf ATTIA</title>
         <meta name="description" content="Oussama Abderraouf ATTIA, 2CP student at ESI Algiers" />
       </Head>
-      <main className="">
-        Home
+      <main className="flex items-center text-dark w-full min-h-screen">
+        <Layout>
+          <div className="flex  items-center justify-between w-full">
+            <div className=''>
+              <Image src={developerPic} alt="Oussama Abderraouf ATTIA" className="w-full h-auto" />
+            </div>
+            <div>
+              <h1>
+
+              </h1>
+              <p>
+                
+              </p>
+            </div>
+          </div>
+        </Layout>
       </main>
     </>
-  )
+  );
 }
